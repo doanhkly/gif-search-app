c95c532050d4cc513101fe6e3702049bfafc58e9
Doanh Ly <ly.do@husky.neu.edu>
2019-11-21 21:15:22 -0800

    changed http to https

--- src/utils/getSearch.js
+++ src/utils/getSearch.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function getSearch(searchTerm, page, limit) {
   const queryString = `q=${searchTerm}&api_key=${process.env.REACT_APP_API_KEY}&limit=${limit}&offset=${page * limit}`
-  const endpoint = `http://api.giphy.com/v1/gifs/search?${queryString}`;
+  const endpoint = `https://api.giphy.com/v1/gifs/search?${queryString}`;
   const result = await axios.get(endpoint);
   const arr = result.data;
 
--- src/utils/getTrending.js
+++ src/utils/getTrending.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function getTrending(page, limit) {
   const queryString = `api_key=${process.env.REACT_APP_API_KEY}&limit=${limit}&offset=${page * limit}`
-  const endpoint = `http://api.giphy.com/v1/gifs/trending?${queryString}`;
+  const endpoint = `https://api.giphy.com/v1/gifs/trending?${queryString}`;
   const result = await axios.get(endpoint);
   const arr = result.data;
 
